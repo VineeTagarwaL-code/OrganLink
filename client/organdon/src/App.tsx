39acb9988d0f8e1b10f6270c8017b98465235073
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-19 20:56:20 +0530

    build: SignUp form completed

--- client/organdon/src/App.tsx
+++ client/organdon/src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
 //pages
 import Signup from "./pages/SignUpPage";
-import Login from "./pages/Login";
+
 function App() {
   return (
     <>
       <Signup />
-      <Login />
     </>
   );
 }
--- client/organdon/src/components/SignUpForm.tsx
+++ client/organdon/src/components/SignUpForm.tsx
@@ -3,61 +3,205 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { SignUpSchema } from "@/schemas/SignUpSchema";
 
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function SignUpForm() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const isLoading = form.formState.isLoading;
+
+  function onSubmit(values: z.infer<typeof SignUpSchema>) {
+    const formData = { ...values };
+    console.log("form data: ", formData);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name="name"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    placeholder="Enter your first name"
+                    {...field}
+                    className="bg-[#2c2c2c]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
         />
-        <Button type="submit">Submit</Button>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    placeholder="abc@gmail.com"
+                    className="bg-[#2c2c2c]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {/*contact number*/}
+        <FormField
+          control={form.control}
+          name="contact"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                <FormLabel>Contact Number</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    placeholder="1234567890"
+                    className="bg-[#2c2c2c]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+
+        {/*state*/}
+        <FormField
+          control={form.control}
+          name="state"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                <FormLabel>State</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    placeholder="West Bengal"
+                    className="bg-[#2c2c2c]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+
+        <div className="flex justify-between w-full">
+          {/*city*/}
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => {
+              return (
+                <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Kolkata"
+                      className="bg-[#2c2c2c]"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          {/*zip code*/}
+          <FormField
+            control={form.control}
+            name="zipCode"
+            render={({ field }) => {
+              return (
+                <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                  <FormLabel>Zip code</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="123456"
+                      {...field}
+                      className="bg-[#2c2c2c] "
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="role"
+          defaultValue="institution"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col items-start text-[#D4D4D4]">
+                <FormLabel>SignUp as</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl className="bg-[#2c2c2c]">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select your role" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="bg-[#D4D4D4]">
+                    <SelectItem value="individual">individual</SelectItem>
+                    <SelectItem value="institution">Institution</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <Button
+          type="submit"
+          className="bg-[#ed5757] font-bold w-full text-lg hover:bg-[#D4D4D4] hover:text-[#ed5757]"
+        >
+          Create account
+        </Button>
       </form>
     </Form>
   );
