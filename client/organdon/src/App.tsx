c86efbfc80fca4a1ec6a33ec48090e9ac2ca41e4
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-19 19:03:32 +0530

    intialised the project

---
+++ client/organdon/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>if you are seeing this means it worked fine</Button>
+    </>
+  );
+}
+
+export default App;
