6a4a712bc405f9d4e62cecdd90acece855003961
Santi Singha <santisingha191@gmail.com>
2024-01-19 20:53:37 +0530

    build: added routes for login and signup

--- client/organdon/src/App.tsx
+++ client/organdon/src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 //pages
 import Signup from "./pages/SignUpPage";
 import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Signup />
-      <Login />
-    </>
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
