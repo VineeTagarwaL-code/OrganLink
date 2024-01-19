48ac67616d6c2e793104f6ceeb3333775d0086da
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-19 21:14:33 +0530

    fix: tailwind fixed & some basic styles updated

--- client/organdon/src/App.tsx
+++ client/organdon/src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 //pages
 import Signup from "./pages/SignUpPage";
 import Login from "./pages/Login";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
+    <div className="">
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
