5c620a9ab5c10baafd8c627dc389ab71a54ab3fe
rahulyadav3766 <rahulyadav3766@gmail.com>
2024-01-20 11:31:10 +0530

    fix: merge commit

--- client/organdon/src/App.tsx
+++ client/organdon/src/App.tsx
@@ -7,7 +7,6 @@ import Admin from "./pages/admin";
 import MyOrgans from "./components/MyOrgans";
 import Announcement from "./pages/Announcement";
 
-
 function App() {
   return (
     <div className=" overflow-hidden">
@@ -15,15 +14,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< Updated upstream
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/myorgans" element={<MyOrgans/>}/>
-=======
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/announcement" element={<Announcement />} />
         <Route path="/myorgans" element={<MyOrgans />} />
->>>>>>> Stashed changes
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
